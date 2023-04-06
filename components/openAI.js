3c063c31a9dfee394b64ce1b5e4fef14154d1ee7
Leonid Shirokov <shirokoff@me.com>
2023-04-06 05:17:21 +0200

    откат

--- components/openAI.js
+++ components/openAI.js
@@ -1,10 +1,9 @@
 const aiUrl = "https://api.openai.com/v1/chat/completions";
-const KEY = process.env.AI_KEY;
 
 export async function openAI(body = null) {
     const headers = {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${KEY}`,
+        Authorization: `Bearer ${secrets.AI_KEY}`,
     };
     const res = await fetch(aiUrl, {
         method: "POST",
